629204037b33793334bdc344ebd7f4d6328cdb31
Toby Ladislas <tladislas@gmail.com>
2017-07-22 04:17:55 -0500

    Add questionnaire for new employees

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+gets.chomp 
+
+puts "How old are you? What year were you born?"
+gets.chomp
+
+puts "Our company cafeteria serves garlic bread.  Should we order some for you?"
+gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+gets.chomp
