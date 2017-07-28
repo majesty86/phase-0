e6dc7b3df5172ca106f0f1f2b58f889d2ff8a46e
Toby Ladislas <tladislas@gmail.com>
2017-07-27 22:56:35 -0500

    Add insertion sort algorithm

---
+++ ruby/insertion_sort.rb
@@ -0,0 +1,24 @@
+# INSERTION-SORT ALGORITHM
+
+# for j <- 2 to length[A]
+  # do key ,- A[j]
+    # Insert A[j] into the sorted sequence A [1..j-1]
+    # i <- j-1
+    # while i > 0 and A[i] > key
+      # do A[i+1] ,- A[i]
+        # i <- i-1
+    # A[i + 1] <- key  
+
+[4, 12, 1, 9, 30, 2]    
+
+[1, 2, 4, 9, 12, 30]
+
+# Create empty result array
+# For each number in the array
+# If result array is empty, put number from loop inside empty result array.
+  # After first loop, array will have '4' inside it.
+# If next number is less 
+
+## Print constantly when trying it out.
+
+# Start with Outermost loop, work your way in.
--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,41 +1,70 @@
-arr = [42, 89, 23, 1]
+# arr = [42, 89, 23, 1]
 
-def search_array(array, int)
-  int_found = false
-  count = 0
-  array.each do |arr_index|
-    if arr_index == int
-      int_found = true
-      return count
-    end  
-    count += 1  
-  end
-  if int_found == false
-    nil
-  end
-end 
+# def search_array(array, int)
+#   int_found = false
+#   count = 0
+#   array.each do |arr_index|
+#     if arr_index == int
+#       int_found = true
+#       return count
+#     end  
+#     count += 1  
+#   end
+#   if int_found == false
+#     nil
+#   end
+# end 
 
-p search_array(arr, 89)
+# p search_array(arr, 89)
 
-p search_array(arr, 1)
+# p search_array(arr, 1)
 
-p search_array(arr, 24)
+# p search_array(arr, 24)
 
-def fib(int) 
-    if int == 0
-        return nil
-    elsif int == 1
-        return [0]
-    elsif int == 2
-        return [0,1]
+# def fib(int) 
+#     if int == 0
+#         return nil
+#     elsif int == 1
+#         return [0]
+#     elsif int == 2
+#         return [0,1]
+#     end
+#     int = int - 2
+#     array = [0,1]
+#           while int > 0
+#               array << array[-1] + array[-2]
+#               int -= 1
+#           end
+#           return array
+# end
+
+# p fib(100)[-1] == 218922995834555169026
+
+def bubblesort ary, new_array
+    i = 0
+    if ary.length == 0
+        return new_array
+    else
+        ary.each do |number|
+            
+            
+            puts i
+            next_number = ary[i+1]
+            if next_number == nil
+                p new_array
+                p ary
+                return bubblesort ary, new_array << ary.pop
+            
+
+            elsif number > ary[i+1]
+                ary[i+1] = number
+                ary[i] = next_number
+            end
+            i+=1
+
+
+        end
     end
-    int = int - 2
-    array = [0,1]
-          while int > 0
-              array << array[-1] + array[-2]
-              int -= 1
-          end
-          return array
 end
 
-p fib(100)[-1] == 218922995834555169026
+p bubblesort([0, 30, 50, 88, 3, 100, 20, 1 , 3,2], []) 
