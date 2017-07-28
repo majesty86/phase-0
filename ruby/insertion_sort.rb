d2325e6aed393ccc7422c925fc1d366bd9ee1dda
Toby Ladislas <tladislas@gmail.com>
2017-07-27 23:22:17 -0500

    Pseudocode insertion sorting algorithm

--- ruby/insertion_sort.rb
+++ ruby/insertion_sort.rb
@@ -1,24 +1,37 @@
 # INSERTION-SORT ALGORITHM
 
-# for j <- 2 to length[A]
-  # do key ,- A[j]
-    # Insert A[j] into the sorted sequence A [1..j-1]
-    # i <- j-1
-    # while i > 0 and A[i] > key
-      # do A[i+1] ,- A[i]
-        # i <- i-1
-    # A[i + 1] <- key  
+# Create empty result array called new_array
+
+# Using all integers from first_array (one at a time) loop for each integer in new_array
+
+# If new_array is empty, put integer from first_array inside new_array.
+  # After first loop, array will have '4' inside it.
+  # After second loop, '12'
+  # After third loop, '1'
+  # After fourth loop, '9'
+  # After fifth loop, '30'
+  # After final loop, '2'
+    #For each of these loops, check integer against other integers to see if they less than or greater than.
+      # If less than integer, put before.
+      # If greater than integer, put after.
+ 
+
+# NOTES FROM OFFICE HOURS: 
+
+# Print constantly when trying it out.
+
+# Start with Outermost loop, work your way in.
 
 [4, 12, 1, 9, 30, 2]    
 
 [1, 2, 4, 9, 12, 30]
 
-# Create empty result array
-# For each number in the array
-# If result array is empty, put number from loop inside empty result array.
-  # After first loop, array will have '4' inside it.
-# If next number is less 
+new_array = []
+first_array = [4, 12, 1, 9, 30, 2]
 
-## Print constantly when trying it out.
+first_array.each do |x, new_ary|
+  if new_ary == 0
+  x << new_ary
+  end
+end 
 
-# Start with Outermost loop, work your way in.
