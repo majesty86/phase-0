12bc17d5dd9353ada807d12d1a9e07e62503daf5
Toby Ladislas <tladislas@gmail.com>
2017-08-01 02:30:44 -0500

    Design Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# CLASS-------------------------------
+# Puppy
+
+# CHARACTERISTICS --------------------
+# Legs: 4
+# Cute: yes
+# Friendly: yes
+# Fur color: varies
+# Name: varies.  Note that most puppy names aren't normal human names, like Dave or Jim. 
+# Ear style: Pointy or floppy
+# 
+# BEHAVIOR ---------------------------
+# Fear of stairs
+# Tail wag
+# Uses kitchen floor as bathroom
+# Destroy (things)
+
