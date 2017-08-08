48687a7b3a0cec7f7aa03421c46e93a2de73bf00
Toby Ladislas <tladislas@gmail.com>
2017-08-08 02:22:21 -0500

    Complete function to reverse word, store result in variable, call variable on a silly condition

--- js/explore.js
+++ js/explore.js
@@ -12,7 +12,16 @@
 // BEFORE you look it up, try the FOR loop!  8:57 pm, Monday 8/7.  Continue this tomorrow.
 
 function reverse_word(word) {
-  console.log(word);
+  var new_word = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    new_word += word[i];
+  }
+    return new_word;
 }
 
-reverse_word("hello")
+
+var non_palindromic_reversed_word = reverse_word("encyclopedia");
+
+if ("apples" != "oranges") {
+  console.log(non_palindromic_reversed_word);
+}
