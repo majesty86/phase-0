5006cc8b20cb5c984baf464b734051e7bd6891f5
Toby Ladislas <tladislas@gmail.com>
2017-08-07 20:59:33 -0500

    Psuedocode reverse string function, test basic functionality

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+// REVERSE STRING FUNCTION
+
+// This function should take a string as a parameter, and when printed to the console,
+// should return the same string, but reversed.
+
+// "hello" should return "olleh".
+
+// Whatever the function returns should be stored in a variable.
+
+// After the function works, implement a boolean - for example, one that prints if a certain condition is true.
+
+// BEFORE you look it up, try the FOR loop!  8:57 pm, Monday 8/7.  Continue this tomorrow.
+
+function reverse_word(word) {
+  console.log(word);
+}
+
+reverse_word("hello")
