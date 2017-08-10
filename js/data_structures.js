dba244bfd647e92fdfae72f0048865e48554f390
Toby Ladislas <tladislas@gmail.com>
2017-08-10 12:11:36 -0500

    Build object from previously created arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,7 +2,15 @@ var colors = ["blue", "green", "purple", "red"];
 
 var horse_names = ["Mr. Ed", "Drogon", "Rhaegal", "Viserion"];
 
-colors.push("white")
+colors.push("white");
 
-horse_names.push("BoJack")
+horse_names.push("BoJack");
+
+var colored_horses = {}
+
+horse_names.forEach(function(value, index) {
+  colored_horses[value] = colors[index];
+});
+
+console.log(colored_horses);
 
