843ce04dbd2c28a86f58100fe1631827c601d4ec
Toby Ladislas <tladislas@gmail.com>
2017-08-13 01:43:54 -0500

    Create 3 objects for the find_match function

--- js/algos.js
+++ js/algos.js
@@ -7,31 +7,39 @@
 // Find the length of each phrase in the array.
 // Compare the phrase lengths in a function, and return the longest one.
 
-var phrase_array = ["call me as asap as possible", "thats what she said", "later guys"];
-
-var phrase_array2 = ["don't let your babies grow up to be cowboys", "fishing in the dark", "a boy named sue"];
-
-// console.log(phrase_array[0].length);
-// console.log(phrase_array[1].length);
-// console.log(phrase_array[2].length);
-
-function longest_phrase(array) {
-    if (array[0].length > (array[1].length && array[2].length)) {
-        console.log(array[0]);
-    } else if ((array[1].length) > (array[0].length) && (array[2].length)) {
-        console.log(array[1]);
-    } else if ((array[2].length) > (array[1].length) && (array[0].length)) {
-        console.log(array[2])     
-    }
-  }
-
-longest_phrase(phrase_array);
-longest_phrase(phrase_array2);
-
 // Release 1
 // Find a Key-Value Match
 
 // This function will take two separate objects and check to see if they share at least one key-value pair.
 
+  // Create three objects with key-value pairs.  Two should have a matching key-value pair, and one shouldn't.
+  // Write a function that states whether these objects have matching key-pair values.
+  // Output from the function should be true or false.
 
+var phrase_array = ["call me as asap as possible", "thats what she said", "later guys"];
+var phrase_array2 = ["don't let your babies grow up to be cowboys", "fishing in the dark", "a boy named sue"];
+
+var guitar = {play_with: 'pick', strings: 6};
+var violin = {play_with: 'bow', strings: 4};
+var ukulele = {play_with: 'fingers', strings: 4};
+
+function longest_phrase(array) {
+  if (array[0].length > (array[1].length && array[2].length)) {
+       console.log(array[0]);
+  } else if ((array[1].length) > (array[0].length) && (array[2].length)) {
+        console.log(array[1]);
+  } else if ((array[2].length) > (array[1].length) && (array[0].length)) {
+        console.log(array[2])     
+    }
+  }
+
+function find_match(obj1, obj2) {
+    
+}
+
+
+// longest_phrase(phrase_array);
+// longest_phrase(phrase_array2);
+
+find_match(violin, ukulele);
 
