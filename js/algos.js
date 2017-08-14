52cb5cd5b0e932211a2fc2c4b5788d5832e1a9c0
Toby Ladislas <tladislas@gmail.com>
2017-08-14 02:28:59 -0500

    Refactor longest phrase function, add and complete random test data function

--- js/algos.js
+++ js/algos.js
@@ -23,28 +23,59 @@ var guitar = {play_with: 'pick', strings: 6};
 var violin = {play_with: 'bow', strings: 4};
 var ukulele = {play_with: 'fingers', strings: 4};
 
+// Initially, I psuedocoded and created this myself, and it worked for release 0, but only took three strings.  
+
+// function longest_phrase(array) {
+//   if (array[0].length > (array[1].length && array[2].length)) {
+//         console.log(array[0]);
+//   } else if ((array[1].length) > (array[0].length) && (array[2].length)) {
+//         console.log(array[1]);
+//   } else if ((array[2].length) > (array[1].length) && (array[0].length)) {
+//         console.log(array[2])     
+//     }
+//   }
+
+// Refactored into this instead:
+
 function longest_phrase(array) {
-  if (array[0].length > (array[1].length && array[2].length)) {
-       console.log(array[0]);
-  } else if ((array[1].length) > (array[0].length) && (array[2].length)) {
-        console.log(array[1]);
-  } else if ((array[2].length) > (array[1].length) && (array[0].length)) {
-        console.log(array[2])     
+  var largest = 0;
+  var longeststring = " ";
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > largest) {
+      largest = array[i].length;
+      longestword = array[i];
     }
   }
+  console.log(longestword)
+}
+
 
 function find_match(obj1, obj2) {
-    for(key in obj1) {
-      pair1 = (key + " " + obj1[key]) 
-    }
-    for(key in obj2) {
-      pair2 = (key + " " + obj2[key])
-    }
-        if (pair1 == pair2)
-          console.log(true);
-        else 
-          console.log(false);
+  for(key in obj1) {
+    pair1 = (key + " " + obj1[key]) 
   }
+  for(key in obj2) {
+    pair2 = (key + " " + obj2[key])
+  }
+    if (pair1 == pair2)
+      console.log(true);
+    else 
+      console.log(false);
+  }
+
+function random_test_data(int) {
+  var words_array = [];
+  var i = words_array.length;
+    while (i < int) {
+  var random_word = (Math.random().toString(36).slice(2, Math.floor(Math.random() * 10) + 1)); {
+      words_array.push(random_word);
+      i++;
+    }
+  }
+      console.log(words_array);
+      longest_phrase(words_array);
+}
 
 
 
@@ -52,9 +83,65 @@ function find_match(obj1, obj2) {
 // longest_phrase(phrase_array);
 // longest_phrase(phrase_array2);
 
-find_match(ukulele, violin);
-find_match(ukulele, guitar);
-find_match(violin, guitar);
+// find_match(ukulele, violin);
+// find_match(ukulele, guitar);
+// find_match(violin, guitar);
 
+console.log("~~~~~~~~~~")
+console.log("Test 1")
+console.log("~~~~~~~~~~")
+random_test_data(2);
 
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 2")
+console.log("~~~~~~~~~~")
+random_test_data(3);
 
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 3")
+console.log("~~~~~~~~~~")
+random_test_data(4);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 4")
+console.log("~~~~~~~~~~")
+random_test_data(5);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 5")
+console.log("~~~~~~~~~~")
+random_test_data(6);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 6")
+console.log("~~~~~~~~~~")
+random_test_data(7);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 7")
+console.log("~~~~~~~~~~")
+random_test_data(8);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 8")
+console.log("~~~~~~~~~~")
+random_test_data(9);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 9")
+console.log("~~~~~~~~~~")
+random_test_data(10);
+
+console.log(" ");
+console.log("~~~~~~~~~~")
+console.log("Test 10")
+console.log("~~~~~~~~~~")
+random_test_data(11);
